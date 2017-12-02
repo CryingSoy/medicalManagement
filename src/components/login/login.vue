4ff8f8f6987dbae9d59b6d1fda135e201c8fc76f
rc <466403866@qq.com>
2017-12-03 00:45:17 +0800

    update

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,5 +1,9 @@
 <template>
-  <el-dialog title="登录窗口" :visible="isShowLoginWindow" :before-close="colseLogin">
+  <el-dialog title="登录窗口" :visible="isShowLoginWindow" :before-close="colseLogin" center>
+    <div slot="footer" class="dialog-footer">
+      <el-button>取 消</el-button>
+      <el-button class="success-btn" type="success">确 定</el-button>
+    </div>
   </el-dialog>
 </template>
 
@@ -19,3 +23,10 @@ export default {
   }
 }
 </script>
+
+<style lang="less">
+.success-btn {
+  background-color: #24b4aa;
+  border-color: #24b4aa;
+}
+</style>
